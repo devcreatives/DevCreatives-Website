9e4532f4671b28f86e4f40c0a6739c8e73bc6e15
AsmateSoomro <AsmateSoomro@gmail.com>
2021-01-27 00:17:11 +0500

    Servive page added to website

--- src/components/service.js
+++ src/components/service.js
@@ -1,7 +1,9 @@
 import React,{useState} from 'react'
 import Typography from '@material-ui/core/Typography'
 import useStyles from '../styles'
-import { Card, CardContent, CardMedia, Grid, CardActionArea } from '@material-ui/core';
+import {Grid, } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+
 
 function Service() {
 const classes = useStyles();
@@ -16,50 +18,80 @@ return(
                         <Typography variant="h4" >Dev<strong className={classes.title}>Cr</strong>eatives++ </Typography>
                         <Typography variant="body1">IT Solutions</Typography>  
                     </Grid>
-                
-                    <div className={classes.siteDesc}>
-                        <Grid container direction="row" spacing={4}justify="left" alignContent="stretch"> 
-                            <Grid item lg={157} md={300} sm={25} xs={39}>
-                                <Grid container>
-                                    <Grid item>
-                                        <h1>H1 Heading</h1>
-                                        <h2>H2 Heading</h2>        
-                                        <h3>H3 Heading</h3>
-                                        <h4>H4 Heading</h4>
-                                        <h5>H5 Heading</h5>
-                                        <h6>H6 Heading</h6>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </div>
-                            <Grid item lg={157} md={29} sm={25} xs={65}>
-                                <Grid container alignItems="center">
-                                    <Grid item>
-                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-
-                            <Grid  item lg={8} md={6} sm={12} xs={12} >
-                        <Card  className={classes.imageBox} >
-                            
-                                <CardMedia
-                                    component="img"
-                                    image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-                                    className={classes.image}
-                                />
-                            
-                        </Card>
-                    </Grid>
-
-
                 </Grid>
             </Grid>
         </Grid>
+        <div>
+            <Grid container spacing={3}>
+                <Grid item xs={7}>
+                    <Paper className={classes.paper}>
+                    <div id="mainarea">
+				   
+						<h1>H1 Heading</h1>
+						<h2>H2 Heading</h2>
+						<h3>H3 Heading</h3>
+						<h4>H4 Heading</h4>
+						<h5>H5 Heading</h5>
+						<h6>H6 Heading</h6>
+				
+					<p>
+						<a href="#"><img height="107" align="right" border="0" width="165" src="https://material-ui.com/static/images/cards/contemplative-reptile.jpg" alt="image1" class="floatright"/></a>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque, erat dictum finibus euismod, magna enim tristique lacus, eu mollis risus ex at justo. Nullam id mauris et velit finibus posuere. Vestibulum lobortis cursus venenatis. Donec feugiat magna et ex interdum, vel elementum diam tristique. Maecenas augue turpis, fermentum vel auctor at, consectetur ac lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac ligula nec leo ornare iaculis vitae nec ante. Sed aliquam turpis id purus convallis luctus. Maecenas congue tempor dapibus. Cras at placerat sapien. Donec vitae tellus sagittis, pulvinar nisi a, eleifend nibh. Donec at felis consequat turpis volutpat lacinia. In tristique risus et ipsum rhoncus finibus. Integer mattis nisl vitae hendrerit aliquet. Pellentesque ac odio sapien.			
+					</p>
+					<br class="clear"/>
+					<p>
+						<a href="#"><img height="107" align="left" border="0" width="165" src="https://material-ui.com/static/images/cards/contemplative-reptile.jpg" alt="image1" class="floatleft"/></a>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque, erat dictum finibus euismod, magna enim tristique lacus, eu mollis risus ex at justo. Nullam id mauris et velit finibus posuere. Vestibulum lobortis cursus venenatis. Donec feugiat magna et ex interdum, vel elementum diam tristique. Maecenas augue turpis, fermentum vel auctor at, consectetur ac lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac ligula nec leo ornare iaculis vitae nec ante. Sed aliquam turpis id purus convallis luctus. Maecenas congue tempor dapibus. Cras at placerat sapien. Donec vitae tellus sagittis, pulvinar nisi a, eleifend nibh. Donec at felis consequat turpis volutpat lacinia. In tristique risus et ipsum rhoncus finibus. Integer mattis nisl vitae hendrerit aliquet. Pellentesque ac odio sapien.		
+					</p>
+					<br class="clear"/>
+					<p class="center"><a href="#"><img height="107" align="center" border="0" width="165" src="https://material-ui.com/static/images/cards/contemplative-reptile.jpg" alt="image1"/></a></p>			
+					<p class="center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>                        
+				</div>
+                    </Paper>
+                </Grid>
+                <Grid item xs={5} container spacing={3}>
+                    <Paper className={classes.paper}>
+                    <div class="floatright" id="sidearea">
+					  <p><strong>This is strong text</strong><br/>
+						<em>This is emphasised text</em><br/>
+						<acronym>This is an acronym or abbreviation</acronym><br/>
+						<del>This is deleted text</del><br/>
+						<a href="#">This is a link</a><br/>
+						<code>This is code</code><br/></p>
+						<ul>
+							<li><a href="#">Unordered list with link</a>
+								<ul>
+									<li>List 1</li>
+									<li>List 2</li>
+								</ul>
+							</li>
+							<li>Item 1</li>
+							<li>Item 2</li>
+						</ul>
+						<br/>
+						<ol>
+						<li><a href="#">Ordered list with link</a>
+							<ol>
+								<li>Aliquam tincidunt mauris eu risus.</li>
+								<li>Vestibulum auctor dapibus neque.</li>
+							</ol>
+						</li>
+						<li>Aliquam tincidunt mauris eu risus.</li>
+						<li>Vestibulum auctor dapibus neque.</li>
+					</ol>
+					<br/>
+					<dl>
+						<dt>Definition Title</dt>
+						<dd>Definition description goes here......</dd>
+					</dl><br/>                         
+				</div>
+		{/*<!-- Sidearea Ends -->*/}    
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
     </div>
 </div>
-
 )
 }
 export default Service;
