b149aa8be0c965151864b62fc9d06349a9657cd7
Rashid Wassan <rashidwassaan@gmail.com>
2021-01-27 20:49:51 +0500

    done with contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,113 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Grid, Divider } from "@material-ui/core";
+
+import Typography from "@material-ui/core/Typography";
+import useStyles from "../styles";
 
 function contact() {
-    return (
-        <div>
-            This is Contact Page
-        </div>
-    )
+  return (
+    <container>
+      <UpperLogoContainer />
+      <ContactBottomGridSegment />
+    </container>
+  );
+}
+
+function UpperLogoContainer() {
+  const classes = useStyles();
+  return (
+    <div id="logoTextOnContactPage">
+      <Typography variant="h3">
+        Sa<strong className={classes.title}>nt</strong>ao{" "}
+      </Typography>
+      <Typography variant="body2">SOFT CONSULTING</Typography>
+    </div>
+  );
+}
+
+function ContactBottomGridSegment() {
+  const classes = useStyles();
+  return (
+    <div className="contactPageBottomContent">
+      <Grid container direction="row" justify="space-between" spacing="1">
+        <Grid item lg={7} md={7}>
+          <Grid container alignItems="center">
+            <Grid item>
+              <Typography variant="h5">Contact Us</Typography>
+              <div className={classes.contactUsParagraph}>
+                <Typography variant="h7">
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Aenean ultricies mi vitae
+                  est. Mauris placerat eleifend leo.
+                </Typography>
+
+                <Typography className={classes.inputFieldTitle}>
+                  <strong>Name :</strong>
+                </Typography>
+
+                <input className={classes.contactInputField}></input>
+
+                <Typography className={classes.inputFieldTitle}>
+                  <strong>E-Mail :</strong>
+                </Typography>
+                <input className={classes.contactInputField}></input>
+                <Typography className={classes.inputFieldTitle}>
+                  <strong>Message :</strong>
+                </Typography>
+                <textarea className={classes.contactInputField}></textarea>
+                <div>
+                  <button className={classes.contactUsSubmitButton}>
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item lg={4} md={4}>
+          <Typography variant="h5">Sales Inquiries</Typography>
+          <div className={classes.contactUsSalesInquiriesPara}>
+            <Typography variant="h7">
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas.
+            </Typography>
+            <Typography className={classes.contactUsSalesInquiriesParaEmail}>
+              <strong>sales@yourdomainname.com</strong>
+            </Typography>
+          </div>
+          <Typography variant="h5">Technical Support</Typography>
+          <div className={classes.contactUsSalesInquiriesPara}>
+            <Typography variant="h7">
+              Current customers contact our technical support team.
+            </Typography>
+            <Typography className={classes.contactUsSalesInquiriesParaEmail}>
+              <strong>support@yourdomainname.com</strong>
+            </Typography>
+          </div>
+          <Typography variant="h5">Biz Development</Typography>
+          <div className={classes.contactUsSalesInquiriesPara}>
+            <Typography variant="h7">
+              Contact our business development team today!
+            </Typography>
+            <Typography className={classes.contactUsSalesInquiriesParaEmail}>
+              <strong>business@yourdomainname.com</strong>
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+      <Divider className={classes.divider}></Divider>
+
+      <Grid container direction="row" justify="space-between" spacing="3">
+        <p className={classes.footerText}>
+          Copyright &copy; 2009 Evardo Web Consulting. Designed by Rashid Wassan{" "}
+        </p>
+        <p className={classes.footerText}>
+          Terms & Condition | Privacy Policy | Sitemap
+        </p>
+      </Grid>
+    </div>
+  );
 }
 
 export default contact;
--- src/styles.js
+++ src/styles.js
@@ -1,92 +1,127 @@
-import {makeStyles} from '@material-ui/core/styles';
-import box from './components/messageBox.jpg';
- 
+import { makeStyles } from "@material-ui/core/styles";
+import box from "./components/messageBox.jpg";
+
 const UseStyles = makeStyles((theme) => ({
-    root:{  
-       background:'white'
-       
-    },   
-    header:{
-        fontSize:14,
-        fontWeight:'30px',
-        
-        borderRadius:'20px',
-        border:10,
-        boxShadow:'0 3px 3px',
-        background:'transparent'
-        
-        
-    },
-    appbar:{
-        border:'0'
-    },
-    homeContent:{
-        border:5,
-    },
-    brand:{
-        color:'white'
-    },
-    detailsPart:{
-        topMargin:'10px',
-        padding:'auto',
-        width:'100%',
-        height:'auto'
-    },
+  root: {
+    background: "white",
+  },
+  header: {
+    fontSize: 14,
+    fontWeight: "30px",
 
-    title:{
-        color:'green'
-    },
-    webName:{
-        marginLeft:theme.spacing(10),
-        marginTop:theme.spacing(10),
-        fontSize:'50px'
-    },
-    imageBox:{
-        marginTop:theme.spacing(5),
-        blockSize:'fit-content',
-    },
-    image:{
-        marginTop:theme.spacing(5),
-        height:'auto',
-        width:'100%'
-    },
-    imageDetails:{
-        maxWidth:'fit-content',
-        blockSize:'fit-content',
-        height:'50px',
-        marginTop:theme.spacing(5),
-        marginLeft:theme.spacing(5),
-    },
-    gridItem:{
-        maxWidth:'fit-content',
-        blockSize:'fit-content'
-    },
-    siteDesc:{
-        marginTop:theme.spacing(2),
-        padding:theme.spacing(2),
+    borderRadius: "20px",
+    border: 10,
+    boxShadow: "0 3px 3px",
+    background: "transparent",
+  },
+  appbar: {
+    border: "0",
+  },
+  homeContent: {
+    border: 5,
+  },
+  brand: {
+    color: "white",
+  },
+  detailsPart: {
+    topMargin: "10px",
+    padding: "auto",
+    width: "100%",
+    height: "auto",
+  },
 
-        width:'100%',
-        height:'auto'
-    },
-    news:{
-        marginTop:theme.spacing(2)
-    
-    },
-    credits:{
-        display:'flex',
-        flexDirection:'column'
-    },
-    creditsImage:{
-        marginTop:theme.spacing(4),
-        width:'40%',
-        height:'40%'
-    },
-    creditsItem:{
-        display:'flex'
+  title: {
+    color: "green",
+  },
+  webName: {
+    marginLeft: theme.spacing(10),
+    marginTop: theme.spacing(10),
+    fontSize: "50px",
+  },
+  imageBox: {
+    marginTop: theme.spacing(5),
+    blockSize: "fit-content",
+  },
+  image: {
+    marginTop: theme.spacing(5),
+    height: "auto",
+    width: "100%",
+  },
+  imageDetails: {
+    maxWidth: "fit-content",
+    blockSize: "fit-content",
+    height: "50px",
+    marginTop: theme.spacing(5),
+    marginLeft: theme.spacing(5),
+  },
+  gridItem: {
+    maxWidth: "fit-content",
+    blockSize: "fit-content",
+  },
+  siteDesc: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(2),
 
-    }
-    
+    width: "100%",
+    height: "auto",
+  },
+  news: {
+    marginTop: theme.spacing(2),
+  },
+  credits: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  creditsImage: {
+    marginTop: theme.spacing(4),
+    width: "40%",
+    height: "40%",
+  },
+  creditsItem: {
+    display: "flex",
+  },
+
+  contactUsParagraph: {
+    marginTop: theme.spacing(2),
+  },
+
+  contactUsSalesInquiriesPara: {
+    marginTop: theme.spacing(1),
+  },
+
+  contactUsSalesInquiriesParaEmail: {
+    marginTop: theme.spacing(0.5),
+    marginBottom: theme.spacing(4),
+  },
+
+  inputFieldTitle: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+  },
+
+  contactUsSubmitButton: {
+    backgroundColor: "black",
+    padding: "4px 12px",
+
+    marginTop: theme.spacing(2),
+    color: "white",
+    border: "none",
+    fontWeight: "bold",
+  },
+  divider: {
+    marginTop: theme.spacing(0),
+    marginBottom: theme.spacing(2),
+  },
+  contactInputField: {
+    width: "300",
+  },
+
+  footerText: {
+    fontSize: 11,
+    fontWeight: "bold",
+    marginTop: theme.spacing(2),
+    display: "inLine",
+  },
 }));
-
 
 export default UseStyles;
